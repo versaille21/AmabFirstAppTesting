5714275970c5bf374702706b99b3d9c844871f09
manizan <angeassoua21@gmail.com>
2019-05-03 15:36:41 +0530

    homeActivity Added

---
+++ app/src/main/java/ci/dafp/finances/amabfirstapptesting/HomeActivity.java
@@ -0,0 +1,13 @@
+package ci.dafp.finances.amabfirstapptesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
